644e3affc9d6b6884044361d6da6a090ff006257
JH-TT <79801565+JH-TT@users.noreply.github.com>
2021-03-02 01:52:10 +0900

    modified 1:52

--- BaekJoon/list(1)/min, max.py
+++ BaekJoon/list(1)/min, max.py
@@ -1,5 +1,4 @@
 a = int(input())  # 몇개의 정수를 입력할지
 array = list(map(int, input().split())) # 공백을 구분해 입력한 정수들을 리스트로 선언.
 
-array.sort() # 배열을 오름차순으로 정렬한다.
 print(min(array), max(array)) # min은 최솟값을, max는 최댓값을 출력한다.
