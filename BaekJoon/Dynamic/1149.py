bf4a2bda233e02b4bd343a871a8b5d0028af5e43
dlwhdgh <>
2021-04-24 18:31:51 +0000

    4/25수정

--- BaekJoon/Dynamic/1149.py
+++ BaekJoon/Dynamic/1149.py
@@ -13,31 +13,3 @@ for _ in range(1, n):
     rst.append((R, G, B))
 fR, fG, fB = rst[-1] # 마지막 RGB출력.
 print(min(fR, fG, fB)) # 그 중 최솟값 출력.
-
-# 이건 왜 안될까?
-n = int(input())
-
-cost = []
-result = []
-for _ in range(n):
-    cost.append(list(map(int, input().split())))
-
-for i in range(3):
-    sum = cost[0][i]
-    k = i
-    for j in range(1, n):
-        if k == 0:
-            a = min(cost[j][1], cost[j][2])
-            sum += a
-            k = cost[j].index(a)
-        elif k == 2:
-            a = min(cost[j][0], cost[j][1])
-            sum += a
-            k = cost[j].index(a)
-        else:
-            a = min(cost[j][0], cost[j][2])
-            sum += a
-            k = cost[j].index(a)
-    result.append(sum)
-print(min(result))
-# 뭔가 다른 경우들을 무시해 버리는 경우가 있는거 같은데 어떤 경우인지 아직 모름..
