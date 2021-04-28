648830e729cf1b067f4ea74283f00de422ed6c45
dlwhdgh <>
2021-04-28 17:51:25 +0000

    4/29

--- BaekJoon/Dynamic/1932.py
+++ BaekJoon/Dynamic/1932.py
@@ -9,5 +9,5 @@ for _ in range(n):
 
 for i in range(1, n):
     for j in range(1, len(num[i]) - 1):
-        num[i][j] = num[i][j] + max(num[i - 1][j - 1], num[i - 1][j])
+        num[i][j] = num[i][j] + max(num[i - 1][j - 1], num[i - 1][j]) # 왼쪽 대각선이랑 오른쪽 대각선 중에 더 큰 값을 더한다.
 print(max(num[n - 1]))
