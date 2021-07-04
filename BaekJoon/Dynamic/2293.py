bffdf777ff9a124c0b0246876fe0d840ff6f72e5
dlwhdgh <79801565+JH-TT@users.noreply.github.com>
2021-07-04 19:00:03 +0000

    7/5

---
+++ BaekJoon/Dynamic/2293.py
@@ -0,0 +1,12 @@
+n, k = map(int, input().split())
+
+coin = [int(input()) for _ in range(n)]
+d = [0] * (k + 1)
+d[0] = 1
+for i in coin:
+    for j in range(1, k + 1):
+        if j - i >= 0:
+            d[j] += d[j - i]
+print(d[k])
+
+# https://mong9data.tistory.com/68 참고.
---
+++ Programmers/SQL/59034.sql
@@ -0,0 +1,3 @@
+-- 모든 레코드 조회하기
+SELECT * FROM ANIMAL_INS ORDER BY ANIMAL_ID
+-- 아이디순서에 따라 행으로 검색.
