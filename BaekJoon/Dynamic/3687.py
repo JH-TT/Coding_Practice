b26aafb3c8d20acef144df7a0930818125a84a8b
dlwhdgh <79801565+JH-TT@users.noreply.github.com>
2021-09-09 19:51:11 +0000

    9/10 Greedy+DP(성냥개비) 내용추가

--- BaekJoon/Dynamic/3687.py
+++ BaekJoon/Dynamic/3687.py
@@ -35,3 +35,33 @@ for n in range(T):
     n = int(input())
     
     print(res[n][0], res[n][1])
+
+
+# 다른 사람의 풀이
+from sys import stdin as f
+
+T = int(f.readline())
+num = [0,0,1,7,4,2,0,8,10] # 가장 앞자리만 6이고 나머지는 0이므로 인덱스 6의 값이 0인것이다.
+dp = [-1 for i in range(101)]
+dp[0] = 0
+dp[1] = 1
+dp[2] = 1
+dp[3] = 7
+dp[4] = 4
+dp[5] = 2
+dp[6] = 6
+dp[7] = 8
+
+
+def find_min(n):
+    if dp[n] == -1:
+        dp[n] = min([10*find_min(n-i)+num[i] for i in range(2,8)])
+    return dp[n]
+
+def find_max(n):
+    res = '1' * (n//2 - 1)
+    return '7'+res if n % 2 else '1'+res
+for test_case in range(T):
+    n = int(f.readline())
+    print(find_min(n),find_max(n))
+# 이 풀이는 최솟값을 2 ~ 7개가 남는다는 가정으로 경우의 수를 모두 구해서 그 중에 최솟값을 최솟값으로 지정했다.        
