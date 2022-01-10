f419566e7c9526452722b68db85ffd842b070f13
dlwhdgh <79801565+JH-TT@users.noreply.github.com>
2022-01-10 08:30:00 +0000

    1/10 String_P(60057 - 문자열 압축) 내용추가

--- Programmers/String_P/60057.py
+++ Programmers/String_P/60057.py
@@ -38,3 +38,23 @@ def solution(s):
         answer = min(answer, count_char(d))
     
     return answer
+
+
+# 다른 풀이
+
+def compress(text, tok_len):
+    words = [text[i:i+tok_len] for i in range(0, len(text), tok_len)] # 범위를 벗어나도 끝에서 자른다
+    res = []
+    cur_word = words[0]
+    cur_cnt = 1
+    for a, b in zip(words, words[1:] + ['']):
+        if a == b:
+            cur_cnt += 1
+        else:
+            res.append([cur_word, cur_cnt])
+            cur_word = b
+            cur_cnt = 1
+    return sum(len(word) + (len(str(cnt)) if cnt > 1 else 0) for word, cnt in res) # min 함수 안에는 for문을 이용할 수 있다.
+
+def solution(text):
+    return min(compress(text, tok_len) for tok_len in list(range(1, int(len(text)/2) + 1)) + [len(text)])
