9d873d8011fa08884d9d022f7ec4b1c56b9ff9a4
repl.it user <replituser@example.com>
2025-02-17 16:03:38 +0000

    Auto-commit: 2025-02-17 16:03:38

--- watch.sh
+++ watch.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-WATCH_DIR="./"  # Replit 프로젝트 폴더 감시
+WATCH_DIR="./"  # 감시할 디렉토리
 
-while inotifywait -r -e create,modify "$WATCH_DIR"; do
+# 제외할 파일/디렉토리 패턴 (정규 표현식 사용)
+EXCLUDE_PATTERN="(\.git/|watch\.log|node_modules/|.*\.tmp)"
+
+while inotifywait -r -e create,modify --exclude "$EXCLUDE_PATTERN" "$WATCH_DIR"; do
     git add .
     git commit -m "Auto-commit: $(date +'%Y-%m-%d %H:%M:%S')"
-    git push origin master  # 브랜치 이름이 main이 아닐 경우 수정
+    git push origin main
 done
