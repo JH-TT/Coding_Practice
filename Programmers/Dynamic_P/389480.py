7bd6d674d7ee1855dad12744b1dfa7e5f1d1184c
JH-TT <79801565+JH-TT@users.noreply.github.com>
2025-02-20 16:35:52 +0000

    2025-02-21 Dynamic_P (389480 - [2025 프로그래머스 코드챌린지 2차 예선] 완전범죄) 수정

--- Programmers/Dynamic_P/389480.py
+++ Programmers/Dynamic_P/389480.py
@@ -15,3 +15,34 @@ def solution(info, n, m):
   return min(dp[-1]) if min(dp[-1]) < n else -1
 
 # dp[x][y]: x개의 보석을 훔쳤을때 B도둑의 흔적이 y개일때 A도둑의 흔적 최소 개수
+
+# BFS 풀이
+from collections import deque
+
+def solution(info, n, m):
+    q = deque()
+    q.append((0, 0, 0))
+    visit = set() # 훔친 횟수와 a b 흔적 개수가 같은 경우를 제외하기 위한 방문처리
+
+    a_evi = []
+    while q:
+        cnt, a, b = q.popleft()
+
+        if (cnt, a, b) in visit:
+            continue
+
+        if a >= n or b >= m:
+            continue
+
+        if cnt == len(info):
+            a_evi.append(a)
+            continue
+
+        visit.add((cnt, a, b))
+        q.append((cnt + 1, a + info[cnt][0], b))
+        q.append((cnt + 1, a, b + info[cnt][1]))
+
+    if len(a_evi) == 0 or min(a_evi) >= n:
+        return -1
+
+    return min(a_evi)
