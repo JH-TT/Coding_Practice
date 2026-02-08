7e75d442eb3bef494a919dd61c6c7b7a4e85ce0c
JH-TT <leejongho9803@gmail.com>
2026-02-08 22:49:31 +0900

    2026-02-08 (9205 - 맥주 마시면서 걸어가기) 코드 리뷰 반영
    [수정 사항]
    - Queue<int[]> -> Queue<Integer>: 좌표 배열 대신 인덱스 기반 BFS로 변경
    - p == pos[N+1] -> i == N+1: 배열 참조(==) 비교를 인덱스 비교로 수정
    - int[] visit -> boolean[] visit: 방문 여부에 맞는 타입으로 변경
    - TODO 주석 제거

--- BaekJoon/DFS_BFS/9205.java
+++ BaekJoon/DFS_BFS/9205.java
@@ -13,7 +13,7 @@ public class Main {
     static int T;
     static int N;
     static int[][] pos; // 0: 집, 1~N: 편의점, N+1: 페스티벌
-    static int[] visit; // 방문여부
+    static boolean[] visit; // 방문여부
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,7 +24,7 @@ public class Main {
         for (int t = 0; t < T; t++) {
             N = Integer.parseInt(br.readLine());
             pos = new int[N + 2][2];
-            visit = new int[N + 2];
+            visit = new boolean[N + 2];
 
             for (int i = 0; i < N + 2; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
@@ -32,32 +32,30 @@ public class Main {
                 pos[i][1] = Integer.parseInt(st.nextToken());
             }
 
-            // TODO: BFS/DFS로 집(0)에서 페스티벌(N+1)까지 도달 가능한지 판별
-            Queue<int[]> q = new LinkedList<>();
-            q.add(pos[0]);
-            visit[0] = 1;
+            // BFS로 집(0)에서 페스티벌(N+1)까지 도달 가능한지 판별
+            Queue<Integer> q = new LinkedList<>();
+            q.add(0);
+            visit[0] = true;
 
             boolean check = false;
             while (!q.isEmpty()) {
-                int[] now = q.poll();
+                int nowIdx = q.poll();
 
                 for (int i = 1; i < N + 2; i++) {
-                    if (visit[i] == 1) continue;
+                    if (visit[i]) continue;
 
-                    int[] p = pos[i];
-                    int dist = Math.abs(now[0] - p[0]) + Math.abs(now[1] - p[1]);
+                    int dist = Math.abs(pos[nowIdx][0] - pos[i][0]) + Math.abs(pos[nowIdx][1] - pos[i][1]);
 
                     if (dist > 1000) continue;
 
-                    visit[i] = 1;
+                    visit[i] = true;
 
-                    // 도착지면 종료
-                    if (p == pos[N + 1]) {
+                    if (i == N + 1) {
                         check = true;
                         break;
                     }
 
-                    q.add(p);
+                    q.add(i);
                 }
 
                 if (check) break;
